060e0e2f22e2833c23629e11e87e3267a0f450e7
cesarhenrq <cesaarhvb@gmail.com>
2023-06-04 03:02:57 -0300

    feat: add update task status

--- src/app/repositories/tasks.repository.ts
+++ src/app/repositories/tasks.repository.ts
@@ -14,6 +14,10 @@ class TasksRepository {
   async getById(id: string) {
     return await this.model.findById(id);
   }
+
+  async updateStatus(id: string, status: string) {
+    return await this.model.findByIdAndUpdate(id, { status }, { new: true });
+  }
 }
 
 export default TasksRepository;
