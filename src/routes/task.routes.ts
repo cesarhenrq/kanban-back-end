26b4f20d10010c07b09c71f5d0fe5f4a299d2d66
cesarhenrq <cesaarhvb@gmail.com>
2023-06-04 02:23:57 -0300

    feat: add create task route

--- src/routes/task.routes.ts
+++ src/routes/task.routes.ts
@@ -1,5 +1,11 @@
-import {Router} from "express";
+import { Router } from "express";
+
+import TaskModule from "../app/modules/task.module";
 
 const taskRouter = Router();
+
+const taskModule = TaskModule.build();
+
+taskRouter.post("/", taskModule.create.bind(taskModule));
 
 export default taskRouter;
