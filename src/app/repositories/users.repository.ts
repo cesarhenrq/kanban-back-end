61e5be240dc19bbc5ccfa6ba282bf9bef2f7f8dd
cesarhenrq <cesaarhvb@gmail.com>
2023-06-04 01:43:07 -0300

    feat: add create user

--- src/app/repositories/users.repository.ts
+++ src/app/repositories/users.repository.ts
@@ -1,10 +1,14 @@
-import User from "../entities/user.model";
+import User, { IUser } from "../entities/user.model";
 
 class UsersRepository {
   model: typeof User;
 
   constructor(model: typeof User) {
     this.model = model;
+  }
+
+  async create(user: IUser) {
+    return await this.model.create(user);
   }
 }
 
