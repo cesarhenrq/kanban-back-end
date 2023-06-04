61a8ef00b50a93bc9a2548214743f1d8b3a01e88
cesarhenrq <cesaarhvb@gmail.com>
2023-06-04 01:49:19 -0300

    feat: add get user by id

--- src/app/repositories/users.repository.ts
+++ src/app/repositories/users.repository.ts
@@ -10,6 +10,10 @@ class UsersRepository {
   async create(user: IUser) {
     return await this.model.create(user);
   }
+
+  async getById(id: string) {
+    return await this.model.findById(id);
+  }
 }
 
 export default UsersRepository;
