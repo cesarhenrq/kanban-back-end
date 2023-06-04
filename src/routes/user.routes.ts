56cb9bdbdd538427cac7a04105c4182bc1b6d091
cesarhenrq <cesaarhvb@gmail.com>
2023-06-04 01:43:37 -0300

    feat: add create user route

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,5 +1,11 @@
 import { Router } from "express";
 
+import UserModule from "../app/modules/user.module";
+
 const userRouter = Router();
+
+const userModule = UserModule.build();
+
+userRouter.post("/", userModule.create.bind(userModule));
 
 export default userRouter;
