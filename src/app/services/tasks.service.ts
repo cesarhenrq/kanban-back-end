367a05575d4fb4419a4318850b795220b8869b48
cesarhenrq <cesaarhvb@gmail.com>
2023-06-04 02:23:42 -0300

    feat: add create tasks service

--- src/app/services/tasks.service.ts
+++ src/app/services/tasks.service.ts
@@ -1,5 +1,9 @@
+import { object, string } from "yup";
+
 import UsersRepository from "../repositories/users.repository";
 import TasksRepository from "../repositories/tasks.repository";
+
+import { ITask } from "../entities/task.model";
 
 class TasksService {
   private usersRepository: UsersRepository;
@@ -11,6 +15,53 @@ class TasksService {
   ) {
     this.usersRepository = usersRepository;
     this.tasksRepository = tasksRepository;
+  }
+
+  async create(payload: ITask) {
+    try {
+      const schema = object().shape({
+        title: string().required().min(3),
+        content: string().required().min(3),
+        status: string().oneOf(["Pendente", "Fazendo", "Concluido"]),
+        user: string().required(),
+      });
+
+      try {
+        await schema.validate(payload);
+      } catch (error: any) {
+        return {
+          statusCode: 400,
+          message: error.errors,
+          data: null,
+        };
+      }
+
+      const user = await this.usersRepository.getById(payload.user);
+
+      if (!user) {
+        return {
+          statusCode: 404,
+          message: "User not found",
+          data: null,
+        };
+      }
+
+      const task = await this.tasksRepository.create(payload);
+
+      await this.usersRepository.associateTask(user._id, task._id);
+
+      return {
+        statusCode: 201,
+        message: "Task created successfully",
+        data: task,
+      };
+    } catch (error: any) {
+      return {
+        statusCode: 500,
+        message: error.message,
+        data: null,
+      };
+    }
   }
 }
 
